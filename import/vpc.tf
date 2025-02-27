9df86a6339557984a8e56af86b4155cfbf954c11
eksuser <ragupathi121290@gmail.com>
2025-02-27 20:32:02 +0530

    new project

---
+++ import/vpc.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc" "default" {
+  id = "vpc-0fadf55931e7b856c"
+}
+
+
---
+++ price/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source = "/root/ragu_project/Modules/vpc"
+  my_region = var.my_region
+  vpc_cidr   = var.vpc_cidr
+  AZ1 = var.AZ1
+  AZ2 = var.AZ2
+  pub_sub = var.pub_sub
+  pri_sub = var.pri_sub
+  tags    = var.tags
+}
+
