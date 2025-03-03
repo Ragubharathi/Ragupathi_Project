edfb4af1cf2043ead0decb3fa910de7ce1e1fa0f
eksuser <ragupathi121290@gmail.com>
2025-03-03 18:35:59 +0530

    role_arn issue fixed

--- eksproject/eks_cluster.tf
+++ eksproject/eks_cluster.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role_policy_attachment" "eks_policy" {
 
 resource "aws_eks_cluster" "eks_cluster" {
   name = "my_eks_cluster"
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = aws_iam_role.eks-role.arn
   
   vpc_config {
     subnet_ids = var.subnet_ids
