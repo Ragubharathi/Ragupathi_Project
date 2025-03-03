64bbc3959a67735b9041284b27c2bcf18e7c14c3
eksuser <ragupathi121290@gmail.com>
2025-03-03 16:15:13 +0530

    tags all removed

--- price/newsubnet.tf
+++ price/newsubnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "price-public-sub-1" {
     cidr_block                                     = "150.150.3.0/24"
     map_public_ip_on_launch                        = false
     
-    tags_all                                       = {
+    tags                                       = {
         "Name" = "price-public-sub_1"
     }
     
