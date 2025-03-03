b4e0dc01c6210b0f52471e34b547255179c77cf6
eksuser <ragupathi121290@gmail.com>
2025-03-03 16:12:30 +0530

    id removed

--- price/newsubnet.tf
+++ price/newsubnet.tf
@@ -1,7 +1,6 @@
 resource "aws_subnet" "price-public-sub-1" {
     vpc_id                                         = "vpc-02fe99c3d622d3b79"  
     cidr_block                                     = "150.150.3.0/24"
-    id                                             = "subnet-09a036ad51e9c1285"
     map_public_ip_on_launch                        = false
     
     tags_all                                       = {
